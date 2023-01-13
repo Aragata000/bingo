29de00d359da35d02174536192ec7c09fa7a045b
Aragata000 <frankpereirag000@gmail.com>
2023-01-13 20:23:12 +0100

    generated random bingo card

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 1-Carton 5 numeros
     -Mostrar carton
 2-Generar numero random
-    -checkear si el numero ha salido antes
-        -Si: Volvemos al paso 2
 3-Checkear si esta en el carton
     -Si: Marcar numero("X")
     -No: futuro(puntuacion)
@@ -25,25 +23,51 @@ const card = [
 ]
 */
 
+const generateRandomNumbers = (
+  totalNumbers,
+  minRandomValue = 1,
+  maxRandomValue = 90
+) => {
+  const result = [];
+  const availableNumbers = Array.from(
+    { length: maxRandomValue },
+    (_, i) => i + minRandomValue
+  );
+  for (let i = 0; i < totalNumbers; i++) {
+    const randomIndex = Math.floor(Math.random() * availableNumbers.length + 1);
+    result.push(availableNumbers[randomIndex]);
+    availableNumbers.splice(randomIndex, 1);
+  }
+  return result;
+};
+
 const createBingoCard = (totalRow, totalCol) => {
   const card = [];
-  let count = 1;
+  let valuesIndex = 0;
+  const values = generateRandomNumbers(totalRow * totalCol, 1, 90);
+
   for (let rowIndex = 0; rowIndex < totalRow; rowIndex++) {
     const row = [];
+
     for (let colIndex = 0; colIndex < totalCol; colIndex++) {
+      //TODO number aleatorio del 1 - 90
       row.push({
-        number: count,
+        number: values[valuesIndex],
         isMatch: false,
       });
-      count++;
+      valuesIndex++;
     }
     card.push(row);
   }
   return card;
 };
 
+const displayBingoCard = (bingoCard) => {
+  const result = "";
+};
+
 const bingo = () => {
-  const bombo = Array.from({ length: 99 }, (_, i) => i + 1);
+  const bombo = Array.from({ length: 90 }, (_, i) => i + 1);
   const getBingoNumber = () => {
     const bomboIndex = Math.floor(Math.random() * bombo.length + 1);
     const item = bombo.splice(bomboIndex, 1)[0];
@@ -53,7 +77,19 @@ const bingo = () => {
   console.log(bingoNumber, bombo);
 
   const bingoCard = createBingoCard(3, 5);
+
+  const checkForMatching = () => {
+    bingoCard.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell.number === bingoNumber) {
+          cell.isMatch = true;
+        }
+      });
+    });
+  };
   console.log(bingoCard);
+
+  checkForMatching();
 };
 
 bingo();
