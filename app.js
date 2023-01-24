0768b924b02bce8659d313c98585dca92d0ebea3
Aragata000 <frankpereirag000@gmail.com>
2023-01-24 21:06:58 +0100

    Main feautures done

--- app.js
+++ app.js
@@ -29,8 +29,15 @@ const askNameAndGreet = () => {
     name = prompt("Hi, what's your name?");
     name ?? alert("Please, insert name");
   }
-  console.log(`Hi ${name}, welcome to our Bingo game !`);
+  alert(`Hi ${name}, welcome to our Bingo game !`);
   return name;
+};
+
+const getFarewell = (playerName) => {
+  const playAgain = confirm("Do you want to play again?");
+  playAgain
+    ? bingo()
+    : alert(`${playerName} , thanks a lot for playing, see you soon`);
 };
 
 const generateRandomNumbers = (
@@ -105,9 +112,11 @@ const displayBingoCard = (bingoCard) => {
   bingoCard.forEach((row, indexRow) => {
     const reducedRow = row.reduce(
       (accumulator, current) => {
-        accumulator.separators += " ---- ";
-        accumulator.numbers += `  ${current.isMatch ? "x" : current.number}${
-          current.number > 9 && !current.isMatch ? " " : "  "
+        accumulator.separators += "\u2002----\u2002";
+        accumulator.numbers += `\u2002\u2002${
+          current.isMatch ? "x" : current.number
+        }${
+          current.number > 9 && !current.isMatch ? "\u2002" : "\u2002\u2002"
         } `;
         return accumulator;
       },
@@ -120,17 +129,44 @@ const displayBingoCard = (bingoCard) => {
       indexRow === bingoCard.length - 1 ? reducedRow.separators : ""
     }`;
   });
-  console.log(result);
   return result;
 };
 
 const getScoreRules = () => {
-  return "El sistema de puntuacion indicara el porcentaje de aciertos";
+  return (
+    "Ours scoring system: \n" +
+    "There are 90 balls in the drum. If you complete the card in less than 90 turns, you will score 100 points for each less turn it took you to complete it."
+  );
 };
 
-const getFinalScore = (attemptCounter, bingoCardLength) => {
-  return ((bingoCardLength / attemptCounter) * 100).toFixed(2);
+const getFinalScore = (bomboLength) => {
+  return bomboLength * 100;
 };
+
+const saveScores = (playerName, finalScore, scores) => {
+  if (!scores[playerName] || finalScore > scores[playerName])
+    scores[playerName] = finalScore;
+};
+
+const displayRanking = (scores) => {
+  const sortedEntries = Object.entries(scores).sort(
+    ([akey, aValue], [bKey, bValue]) => {
+      return bValue - aValue;
+    }
+  );
+  return sortedEntries.reduce(
+    (accumulator, [currentKey, currentValue], index) => {
+      accumulator += `${index + 1}. ${currentKey}: ${currentValue} \n`;
+      return accumulator;
+    },
+    ""
+  );
+};
+
+const scores = {};
+// const getFinalScore = (attemptCounter, bingoCardLength) => {
+//   return ((bingoCardLength / attemptCounter) * 100).toFixed(2);
+// };
 
 const bingo = () => {
   const playerName = askNameAndGreet();
@@ -151,8 +187,7 @@ const bingo = () => {
 
   while (!bingoCard) {
     bingoCard = createBingoCard(bingoCardRows, bingoCardCols);
-    displayBingoCard(bingoCard);
-    !confirm("Quieres esta tarjeta? \n" + displayBingoCard(bingoCard)) &&
+    !confirm("Do you want this bingo card? \n" + displayBingoCard(bingoCard)) &&
       (bingoCard = null);
   }
 
@@ -167,33 +202,40 @@ const bingo = () => {
   };
 
   let attemptCounter = 0;
-
-  while (!isBingo) {
+  let continuePlaying = true;
+  while (continuePlaying && !isBingo) {
     attemptCounter++;
     const bingoNumber = getBingoNumber();
-    console.log(bingoNumber, bombo);
+    // console.log(bingoNumber, bombo);
+    continuePlaying = confirm(`Number: ${bingoNumber}\n Continue?`);
+    if (continuePlaying) {
+      checkForMatching(bingoNumber);
+      alert(displayBingoCard(bingoCard));
 
-    checkForMatching(bingoNumber);
-
-    displayBingoCard(bingoCard);
-
-    if (isLine) {
-      isBingo = checkBingo(bingoCard);
-      console.log("Bingo: ", isBingo);
-    } else {
-      isLine = checkBingo(bingoCard, true);
-      console.log("Line: ", isLine);
+      if (isLine) {
+        isBingo = checkBingo(bingoCard);
+      } else {
+        isLine = checkBingo(bingoCard, true);
+        isLine && alert("You have made a Line");
+      }
     }
   }
+  if (continuePlaying) {
+    const finalScore = getFinalScore(bombo.length);
 
-  console.log(`Intentos ${attemptCounter}`);
+    saveScores(playerName, finalScore, scores);
 
-  alert(
-    `Porcentaje de aciertos: ${getFinalScore(
-      attemptCounter,
-      bingoCardRows * bingoCardCols
-    )}%`
-  );
+    alert(
+      `${playerName}, you have made bingo in ${attemptCounter} attempts.\n
+     Your final score is: ${finalScore} points\n
+     Ranking: \n ${displayRanking(scores)}`
+    );
+
+    getFarewell(playerName);
+  } else {
+    alert(`Thanks for playing, see you soon\n
+    Ranking: \n ${displayRanking(scores)}`);
+  }
 };
 
 bingo();
